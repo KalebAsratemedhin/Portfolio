509531f95d70e3c025d629e27ebe87e8ec553f88
KalebAsratemedhin <risekab@gmail.com>
2024-12-01 22:16:32 +0300

    fix: update button styling

--- portfolio/app/components/Contact.tsx
+++ portfolio/app/components/Contact.tsx
@@ -1,6 +1,6 @@
 const Contact = () => {
     return (
-      <div id="contact" className="w-full min-h-screen bg-primary flex justify-center items-center p-4">
+      <div id="contact" className="w-full min-h-screen bg-primary flex flex-col justify-center items-center p-4">
         <form className="flex flex-col max-w-[600px] w-full animate-slide-up">
           <div className="pb-8">
             <p className="section-title text-4xl font-bold">Contact</p>
@@ -34,7 +34,7 @@ const Contact = () => {
         <a
         href="/resume-vl.pdf"
         download="Kaleb_Resume.pdf"
-        className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-3 px-6 rounded-lg shadow-md transition-all duration-300"
+        className="bg-[#1a1a1a] hover:border-[#646cff] border border-[#64ffda] text-white font-bold py-3 px-6 rounded-lg shadow-md transition-all duration-300"
       >
         Download Resume
       </a>
